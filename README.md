97a71fae4cc7adb1d8b26bd86f495123b3dfecd8
Alexander Morozov <morozov.a@aviashelf.com>
2018-05-17 17:55:51 +1100

    17.05.18 17:55

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # plaza_98.css
 Nightwave Plaza styles based on contra's windows_98.css
+
+# Features
+  * More accurate styles
+  * Menu bar
+  * Perfect Scrollbar support
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require('gulp'),
     stylus       = require('gulp-stylus'),
     crass        = require('gulp-crass'),
+    base64       = require('gulp-base64'),
     rename       = require('gulp-rename'),
     nib          = require('nib'),
     autoprefixer = require('autoprefixer-stylus');
@@ -15,12 +16,22 @@ gulp.task('default', function(){
             ]
         }))
 
+        .pipe(base64({
+            extensions: ['png'],
+            debug: false
+        }))
+
         // dist
         .pipe(rename('plaza98.css'))
         .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('demo'))
 
         // dist minified
         .pipe(rename('plaza98.min.css'))
         .pipe(crass({ pretty: false }))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/*.styl', ['default']);
 });
